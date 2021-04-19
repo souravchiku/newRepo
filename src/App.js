61cd61cf082df025fe2526b5736ad474307be939
souravchiku <76004164+souravchiku@users.noreply.github.com>
2021-04-19 21:41:31 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello sourav</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Start fksd;f</h2>
     </div>
   );
 }
